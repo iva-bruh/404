83be51b16901ec70289c69e4aac3eab5ca9c8582
pupa1337 <145050746+kmhyzenkorv@users.noreply.github.com>
2024-11-16 14:46:01 +0500

    commit

--- Front/src/components/Header.svelte
+++ Front/src/components/Header.svelte
@@ -59,7 +59,6 @@
         <li><a href="/about">О нас</a></li>
         <li><a href="/services">Услуги</a></li>
         <li><a href="/contact">Контакты</a></li>
-        <li><a href="/contact">Контакты</a></li>
        
       </ul>
     </nav>
